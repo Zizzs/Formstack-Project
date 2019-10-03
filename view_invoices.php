204d4ea9262d796922b0d10bfdb41cff579a6d47
Alex Williams <Zizzs17@gmail.com>
2019-10-02 20:15:26 -0700

    Fixed text bug on view_invoices, google font link added.

--- view_invoices.php
+++ view_invoices.php
@@ -2,6 +2,7 @@
     <head>
         <title>Formstack Project</title>
         <link rel="stylesheet" href="css/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto&display=swap" rel="stylesheet">
     </head>
     <body>
         <div class="header_div">
